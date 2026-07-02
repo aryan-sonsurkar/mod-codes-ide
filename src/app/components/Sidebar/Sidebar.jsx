8914d19f61895bbc22d495962120960a7341ca02
Aryan Sonsurkar <aryansonsurkar87@gmail.com>
2026-07-02 11:05:53 +0530

    feat(ui): implement modern IDE-inspired interface with reusable components

--- src/app/components/Sidebar/Sidebar.jsx
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,15 @@
 import "./Sidebar.css";
+import {House} from "lucide-react";
+import {FolderKanban} from "lucide-react";
+import {Settings} from "lucide-react";
+
 export default function Sidebar() {
   return (
 <div className="sidebar">
       <h4>MODCODES</h4>
-      <a href="/Home">Home</a>
-      <a href="/Projects">Projects</a>
-      <a href="/Settings">Settings</a>
+      <a href="/Home" className="homebtn"><House />  Home</a>
+      <a href="/Projects" className="projectsbtn"><FolderKanban />  Projects</a>
+      <a href="/Settings" className="settingsbtn"><Settings />  Settings</a>
 </div>
   );
 }
--- src/app/components/Workspace/chat-input.jsx
+++ src/app/components/Workspace/chat-input.jsx
@@ -1,10 +1,13 @@
 import "./chat-input.css";
+import {Send} from "lucide-react";
+import {Paperclip} from "lucide-react";
+
 export default function Chatinput() {
   return (
 <div className="chat-input">
       <input  className="input" placeholder="Ask ModCodes anything...      "/>
-      <button className="chat-button">Send</button>
-      <button className="chat-button">Attach</button>
+      <button className="chat-button"><Send /></button>
+      <button className="chat-button"><Paperclip /></button>
 </div>
   );
 }
